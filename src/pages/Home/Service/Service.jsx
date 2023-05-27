48d4c64abc30fb4a35ec1c8543b24906d620eb2e
foisal404 <foisalahmmed2@gmail.com>
2023-05-27 23:32:49 +0600

    sort and search impliment

--- src/pages/Home/Service/Service.jsx
+++ src/pages/Home/Service/Service.jsx
@@ -1,14 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ServiceCard from "./ServiceCard";
 
 const Service = () => {
-    const [services,SetServices]=useState([]);
-    useEffect(()=>{
-        fetch('http://localhost:5000/services')
-        .then(res=>res.json())
-        .then(data=>SetServices(data))
-    },[])
-    // console.log(services)
+  const [asc, setAsc] = useState(true);
+  const [services, SetServices] = useState([]);
+  const refFeild=useRef(null)
+  const [search,setSearch]=useState('')
+  const handleSearch=()=>{
+    setSearch(refFeild.current.value);
+  }
+  useEffect(() => {
+    fetch(`http://localhost:5000/services?sort=${asc ? "asc" : "desc"}&search=${search}`)
+      .then((res) => res.json())
+      .then((data) => SetServices(data));
+  }, [asc,search]);
+  // console.log(services)
   return (
     <div className="text-center my-20">
       <h2 className="text-orange-600 text-sm font-bold">About Us</h2>
@@ -17,10 +23,22 @@ const Service = () => {
         the majority have suffered alteration in some form, by injected humour,
         or randomised <br /> words which dont look even slightly believable.
       </p>
+      <div>
+        <input
+          type="text"
+          placeholder="Search"
+          className="input input-bordered"
+          ref={refFeild}
+        />
+        <button onClick={handleSearch} className="btn">Q</button>
+      </div>
+      <button className="text-red-500" onClick={() => setAsc(!asc)}>
+        {asc ? "Price : high to low" : "Price : low to high"}
+      </button>
       <div className="grid grid-cols-3 gap-6 justify-items-center mt-12">
-        {
-            services.map(service=><ServiceCard key={service._id} service={service}></ServiceCard>)
-        }
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service}></ServiceCard>
+        ))}
       </div>
     </div>
   );
